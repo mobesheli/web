966a33be3dc96f8bac1ac89a833d09623337621e
mobesheli <mohsen.amiribesheli@outlook.com>
2018-02-26 11:27:51 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <title>Welcome to Unit 4</title>
+</head>
 <body>
-<h1> This is our Playground </h1>
+<h1> This is our Playground Nescot Peple </h1>
 <?php
 $name = "Mohsen";
 echo "This is php ". $name;
 ?>
-
 </body>
 </html> 
